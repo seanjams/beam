b2706cd7c374c280f5728b46552075a7ca3bf05d
Sean O'Reilly <seanvoreilly2@gmail.com>
2023-04-09 04:20:33 -0700

    fix color mapping

--- hue.py
+++ hue.py
@@ -1,3 +1,4 @@
+import math
 from phue import Bridge
 
 from config import HUE_BRIDGE_IP, HUE_BRIDGE_USERNAME
@@ -9,27 +10,39 @@ from config import HUE_BRIDGE_IP, HUE_BRIDGE_USERNAME
 #     hex = "#%02X%02X%02X" % (r, g, b)
 #     return hex
 
-# This is only scanning R and G? Need better solution
+# https://gist.github.com/error454/6b94c46d1f7512ffe5ee
+def enhance_color(normalized):
+    if normalized > 0.04045:
+        return math.pow( (normalized + 0.055) / (1.0 + 0.055), 2.4)
+    else:
+        return normalized / 12.92
+
 def convert_hex(hex_code):
     if hex_code.startswith("#"):
         hex_code = hex_code[1:]
-    R = int(hex_code[:2],16)
-    G = int(hex_code[2:4],16)
-    B = int(hex_code[4:6],16)
+    r = int(hex_code[:2],16)
+    g = int(hex_code[2:4],16)
+    b = int(hex_code[4:6],16)
 
-    total = R + G + B
+    r_norm = r / 255.0
+    g_norm = g / 255.0
+    b_norm = b / 255.0
 
-    if R == 0:
-        firstPos = 0
-    else:
-        firstPos = R / total
+    r_final = enhance_color(r_norm)
+    g_final = enhance_color(g_norm)
+    b_final = enhance_color(b_norm)
     
-    if G == 0:
-        secondPos = 0
-    else:
-        secondPos = G / total
+    x = r_final * 0.649926 + g_final * 0.103455 + b_final * 0.197109
+    y = r_final * 0.234327 + g_final * 0.743075 + b_final * 0.022598
+    z = r_final * 0.000000 + g_final * 0.053077 + b_final * 1.035763
 
-    return [firstPos, secondPos]
+    if x + y + z == 0:
+        return (0,0)
+    else:
+        x_final = x / (x + y + z)
+        y_final = y / (x + y + z)
+    
+        return (x_final, y_final)
 
 # cache this
 def get_bridge():
@@ -39,11 +52,12 @@ def get_bridge():
 
 def set_color(bridge, light_id, hex_code):
     color_coords = convert_hex(hex_code)
+    bridge.set_light(light_id, 'bri', 200)
     bridge.set_light(light_id, 'xy', color_coords)
 
 def light_the_beam(light_id=None):
     bridge = get_bridge()
-    hue = "#990099"
+    hue = "#660066"
     set_color(bridge, 1, hue)
     set_color(bridge, 2, hue)
     set_color(bridge, 3, hue)
