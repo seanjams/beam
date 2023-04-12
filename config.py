885c569108d606de28303ee3a9beed7d8255957b
Sean O'Reilly <seanvoreilly2@gmail.com>
2023-04-11 18:17:23 -0700

    update config to throw error if no hue variables found

--- config.py
+++ config.py
@@ -1,11 +1,19 @@
 import os
 
+
+class NoHueBridgeCredentials(Exception):
+    pass
+
 # DB url
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///db.sqlite")
 
 # HUE Config
 HUE_BRIDGE_IP = os.getenv("HUE_BRIDGE_IP")
-HUE_BRIDGE_USERNAME = os.getenv("HUE_BRIDGE_IP")
+HUE_BRIDGE_USERNAME = os.getenv("HUE_BRIDGE_USERNAME")
+if not HUE_BRIDGE_IP or not HUE_BRIDGE_USERNAME:
+    raise NoHueBridgeCredentials(
+        "No HUE_BRIDGE_IP and HUE_BRIDGE_USERNAME variables found in environment. Aborting."
+    )
 
 # set configuration values
 class Config:
